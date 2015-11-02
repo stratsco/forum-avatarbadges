34e1c1a654c387cd6ed499a12a0a2fdceb15c36e
Travis Vocino <travis@vocino.com>
2015-11-02 14:23:44 -0800

    fix name of stylesheet asset

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,8 @@
-# name: discourse-twittercommunity
-# about: Customizations for twittercommunity.com
+# name: forum-customizations
+# about: Customizations for forum.strats.co
 # version: 0.1
-# authors: Neil Lalonde
+# authors: Travis Vocino
+# Credits: Neil Lalonde
 
 register_asset "javascripts/discourse/templates/connectors/poster-avatar-bottom/under-avatar.js.handlebars"
-register_asset "stylesheets/twittercommunity.scss"
+register_asset "stylesheets/forum-customizations.scss"
